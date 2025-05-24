855d276f6f6f3d2ab468372877c97af215dc3f07
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2025-05-24 16:47:33 +0530

    Task 2

---
+++ Task 2 To-Do List/script.js
@@ -0,0 +1,61 @@
+
+
+
+const inputBox = document.getElementById("input-box");
+
+const listContainer = document.getElementById("list-container");
+
+
+const list2 = document.getElementById('input-box')
+if(inputBox != null){
+  inputBox.addEventListener('click ' , function(ad){
+    let m = 4 
+    m++
+  })
+  
+}  
+function addTask()
+{
+      if(inputBox.value === "" ){
+        alert("you must write somthing");
+      }
+      else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+      
+
+      }
+      inputBox.value = "";
+      saveData();
+
+}
+
+listContainer.addEventListener("click", function (e) 
+{
+  if(e.target.tagName === "LI")
+  {
+    e.target.classList.toggle("checked");
+
+  }
+else if(e.target.tagName === "SPAN")
+  {
+    e.target.parentElement.remove();
+  }
+  
+
+  
+}, false);
+
+
+function saveData(){
+  localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showtask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
